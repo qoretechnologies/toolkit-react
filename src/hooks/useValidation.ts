96d44d3ab793ec0c6979362fafe7eaa7addfec11
Foxhoundn <filip.witosz+textace@gmail.com>
2024-04-30 15:49:37 +0200

    Refactor useValidation hook to ignore unused variable

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -1,6 +1,7 @@
 import { TFormFieldType } from '../types/Form';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// @ts-ignore
 export const useValidation = (value: any, type?: TFormFieldType) => {
   // Build validation...
 
