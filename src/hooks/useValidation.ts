02fc007fd29bbd720b0b0836c96041a6cf54843c
Foxhoundn <filip.witosz+textace@gmail.com>
2024-04-30 15:50:46 +0200

    Fix useValidation hook to implement missing functionality

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -1,7 +1,7 @@
 import { TFormFieldType } from '../types/Form';
 
+// @ts-expect-error "need to implement this"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// @ts-ignore
 export const useValidation = (value: any, type?: TFormFieldType) => {
   // Build validation...
 
