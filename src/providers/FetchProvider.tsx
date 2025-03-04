a7d3c5c60fd5e839f39017320a51b3d3746269a2
Foxhoundn <filip.witosz+textace@gmail.com>
2025-03-04 17:09:02 +0100

    Update dependencies and refactor FetchProvider to use hooks for API methods

--- src/providers/FetchProvider.tsx
+++ src/providers/FetchProvider.tsx
@@ -1,4 +1,5 @@
 import { useQueryClient } from '@tanstack/react-query';
+import { useCallback, useMemo } from 'react';
 import { FetchContext, TReqraftContextQueryConfig } from '../contexts/FetchContext';
 import { query } from '../utils/fetch';
 
@@ -9,32 +10,43 @@ export interface IReqraftFetchProviderProps {
 export const ReqraftFetchProvider = ({ children }: IReqraftFetchProviderProps) => {
   const queryClient = useQueryClient();
 
-  async function get<T>(config: TReqraftContextQueryConfig) {
-    return query<T>({ queryClient, ...config, method: 'GET' });
-  }
-
-  async function post<T>(config: TReqraftContextQueryConfig) {
-    return query<T>({ queryClient, ...config, method: 'POST' });
-  }
-
-  async function put<T>(config: TReqraftContextQueryConfig) {
-    return query<T>({ queryClient, ...config, method: 'PUT' });
-  }
-
-  async function del<T>(config: TReqraftContextQueryConfig) {
-    return query<T>({ queryClient, ...config, method: 'DELETE' });
-  }
-
-  return (
-    <FetchContext.Provider
-      value={{
-        get,
-        post,
-        put,
-        del,
-      }}
-    >
-      {children}
-    </FetchContext.Provider>
+  const get = useCallback(
+    <T extends any>(config: TReqraftContextQueryConfig) => {
+      return query<T>({ queryClient, ...config, method: 'GET' });
+    },
+    [queryClient]
   );
+
+  const post = useCallback(
+    <T extends any>(config: TReqraftContextQueryConfig) => {
+      return query<T>({ queryClient, ...config, method: 'POST' });
+    },
+    [queryClient]
+  );
+
+  const put = useCallback(
+    <T extends any>(config: TReqraftContextQueryConfig) => {
+      return query<T>({ queryClient, ...config, method: 'PUT' });
+    },
+    [queryClient]
+  );
+
+  const del = useCallback(
+    <T extends any>(config: TReqraftContextQueryConfig) => {
+      return query<T>({ queryClient, ...config, method: 'DELETE' });
+    },
+    [queryClient]
+  );
+
+  const contextValue = useMemo(
+    () => ({
+      get,
+      post,
+      put,
+      del,
+    }),
+    [get, post, put, del]
+  );
+
+  return <FetchContext.Provider value={contextValue}>{children}</FetchContext.Provider>;
 };
